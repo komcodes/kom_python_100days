f065894977959ecdd4eb7e0e664dd92cc89b437e
komcodes <komalbhatia2408@gmail.com>
2025-10-06 20:45:35 +0530

    Add files via upload

---
+++ py prac day1.py
@@ -0,0 +1,12 @@
+print("Hello " + input("What is your name?") + "!")
+
+#print(len(input("What is your name?")))
+username = input("What is your name?")
+length = len(username)
+print(length)
+
+#Band Name Generator
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?")
+pet = input("What's your pet's name?")
+print("Your band name could be" + city + " " + pet)
